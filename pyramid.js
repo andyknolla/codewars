44c7354469dddcc61c882145d6150f8dab7e5efc
Andy Knolla <andy@techtonicgroup.com>
2017-04-27 22:30:04 -0600

    pyramid

---
+++ pyramid.js
@@ -0,0 +1,31 @@
+function pyramid(num) {
+  var x = 'x', space, result;
+	for(var i=1;i <= num;i++) {
+    space = '';
+    for(var j=num - i;j > 0;j--) {
+    	space = space + " ";
+    }
+  	console.log(space + x + space);
+   // result = space + x + space + "\n";
+    x = x + " x";
+  }
+  return result
+}
+
+function goodPyramid(num) {
+	var x = 'x', space = ' ';
+  space = space.repeat(num-1);;
+  while(num > 0) {
+    console.log(space + x + space);
+    x = x + " x", num--;
+    space = space.slice(1, space.length);
+  }
+}
+
+//pyramid(5);
+//     4____x
+//		 3___x_x
+//     2__x_x__x
+//     1_x x x x
+//      x x x x x
+ goodPyramid(15);

f5ead9575c55c071c2a263a0e7ed2271d999d625
Andy Knolla <andyknolla@gmail.com>
2017-05-31 09:51:31 -0600

    Partial completion of caesar cipher

---
+++ first_variation_on_caesar_cipher/test.js
@@ -0,0 +1,3 @@
+var u = "I should have known that you would have a perfect answer for me!!!"
+var v = ["J vltasl rlhr ", "zdfog odxr ypw", " atasl rlhr p ", "gwkzzyq zntyhv", " lvz wp!!!"]
+Test.assertSimilar(movingShift(u, 1), v)

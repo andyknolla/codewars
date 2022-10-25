56e24e38b59fb9a98065d1e9f03faff68b1551e7
Andy <andyknolla@gmail.com>
2022-10-25 11:23:37 -0600

    Fix a few inconsistencies

--- javaScript/squareEveryDigit.js
+++ javaScript/squareEveryDigit.js
@@ -2,7 +2,8 @@ function squareDigits(num){
   return Number( num.toString().split('').map( num => num**2 ).join('') );
 }
 
-/* DESCRIPTION:
+/* 7kyu
+DESCRIPTION:
 Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
 
 For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
--- javaScript/sumOfOddNumbers.js
+++ javaScript/sumOfOddNumbers.js
@@ -1,5 +1,3 @@
-
-
 function rowSumOddNumbers(n) {
   let num = 1;
   let sum = 0;
